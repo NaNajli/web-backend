4cfaa0f3c4613d2a822feb6547af50da8b31b55a
NaNajli <nadurodriguez2@gmail.com>
2025-02-03 18:31:02 -0300

    Data Validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -9,14 +9,22 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin ))
 
 router.get("/register" , utilities.handleErrors(accountController.buildRegister))
 
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
-
 // Process the registration data
 router.post(
-    "/register",
-    regValidate.registationRules(),
-    regValidate.checkRegData,
-    utilities.handleErrors(accountController.registerAccount)
-  )
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
+
+router.post("/register", utilities.handleErrors(accountController.registerAccount))
+
+// Process the login attempt
+router.post(
+  "/login",
+  (req, res) => {
+    res.status(200).send('login process')
+  }
+)
 
 module.exports = router;
